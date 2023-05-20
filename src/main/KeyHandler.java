786b6da1a07640d5c1cd4a6f54ae776c8647c1c6
phamvutuyetanh <phamvutuyetanh@gmail.com>
2023-05-20 18:20:43 +0700

    add control volume

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -4,9 +4,12 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+	GamePanel gp;
     public boolean upPressed, downPressed, leftPressed,
             rightPressed, enterPressed, pausePressed;
-
+    public KeyHandler(GamePanel gp) {
+    	this.gp = gp;
+    }
     @Override
     public void keyPressed(KeyEvent e) {
 
@@ -28,17 +31,30 @@ public class KeyHandler implements KeyListener {
             enterPressed = true;
         }
         if(code == KeyEvent.VK_P){
-            if(GamePanel.pauseState==false){
-            	GamePanel.pauseState = true;
-                GamePanel.stopSE();
-                
-            }    
-            else{
-                GamePanel.pauseState = false;
-                GamePanel.conitue();
-            } 
-        }
-
+             	GamePanel.pauseState = true;
+                 GamePanel.stopSE();
+                           
+    }
+    if(code==KeyEvent.VK_DOWN) {
+    	if(gp.pauseState == true && gp.pause.getcommandNum() == 0  && gp.ingame.getvolumeSlace()>0) {
+    		gp.ingame.setvolumeSlace(gp.ingame.getvolumeSlace()-1);
+    		gp.music.setvolumeSlace(gp.ingame.getvolumeSlace());
+    		gp.music.setFile(4);
+    		gp.music.play();
+    		System.out.println(gp.music.getvolumeSlace());
+    		
+    	}
+    }
+    if(code==KeyEvent.VK_UP) {
+    	if(gp.pauseState == true && gp.pause.getcommandNum() == 0  && gp.ingame.getvolumeSlace()<5) {
+    		gp.ingame.setvolumeSlace(gp.ingame.getvolumeSlace()+1);
+    		gp.music.setvolumeSlace(gp.ingame.getvolumeSlace());
+    		gp.music.setFile(4);
+    		gp.music.play();	
+    		System.out.println(gp.music.getvolumeSlace());
+    		
+    	}
+    }    
     }
 
     @Override
@@ -63,6 +79,7 @@ public class KeyHandler implements KeyListener {
         if (code == KeyEvent.VK_P) {
             pausePressed = false;
         }
+       
     }
 
     @Override
